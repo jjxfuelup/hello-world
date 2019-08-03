e4c6a45aedebb8079fb972860d58bbaea6760475
jjxfuelup <53119750+jjxfuelup@users.noreply.github.com>
2019-08-03 22:17:53 +0800

    Update hello.php

--- hello.php
+++ hello.php
@@ -1,7 +1,3 @@
-header('content-type:application:json;charset=utf8');  
-header('Access-Control-Allow-Origin:*');  
-header('Access-Control-Allow-Methods:POST');  
-header('Access-Control-Allow-Headers:x-requested-with,content-type');  
 
 @ResponseBody
     @RequestMapping("/getMySeat")
