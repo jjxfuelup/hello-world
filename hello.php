1b51e940951c65cc47af5c3a74967c2e5afc9388
jjxfuelup <53119750+jjxfuelup@users.noreply.github.com>
2019-08-03 22:53:31 +0800

    Update hello.php

--- hello.php
+++ hello.php
@@ -1,13 +1,9 @@
-<?php  
-$ret = array(  
-    'name' => isset($_POST['name'])? $_POST['name'] : '',  
-    'gender' => isset($_POST['gender'])? $_POST['gender'] : ''  
-);  
-  
-header('content-type:application:json;charset=utf8');  
-header('Access-Control-Allow-Origin:*');  
-header('Access-Control-Allow-Methods:POST');  
-header('Access-Control-Allow-Headers:x-requested-with,content-type');  
-  
-echo json_encode($ret);  
-?>
+   @ResponseBody
+    @RequestMapping("/getMySeat")
+    public String getMySeatSuccess(@RequestParam("callback") String callback){
+        Gson gson=new Gson();
+        Map<String,String> map=new HashMap<>();
+        map.put("seat","1_2_06_12");
+        logger.info(callback);
+        return callback+"("+gson.toJson(map)+")";
+    }
