a4c38a0393b373b447ccf1d2d24a3f0d2f51a171
jjxfuelup <53119750+jjxfuelup@users.noreply.github.com>
2019-08-03 22:14:17 +0800

    Update hello.php

--- hello.php
+++ hello.php
@@ -1,4 +1,9 @@
-  @ResponseBody
+header('content-type:application:json;charset=utf8');  
+header('Access-Control-Allow-Origin:*');  
+header('Access-Control-Allow-Methods:POST');  
+header('Access-Control-Allow-Headers:x-requested-with,content-type');  
+
+@ResponseBody
     @RequestMapping("/getMySeat")
     public String getMySeatSuccess(@RequestParam("callback") String callback){
         Gson gson=new Gson();
