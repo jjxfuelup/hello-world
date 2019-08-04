a366fa2c1846f5ce8a6d2026aafa4777da278a1e
jjxfuelup <53119750+jjxfuelup@users.noreply.github.com>
2019-08-04 08:23:25 +0800

    Update user.js

--- user.js
+++ user.js
@@ -1,7 +1,3 @@
-header('Access-Control-Allow-Origin:*');
-header('Access-Control-Allow-Methods:POST');
-header('Access-Control-Allow-Headers：Content-Type, api_key, Authorization');
-
 user([
 {"name":"蒋金鑫","age":"23"},
 {"name":"陈条珍","age":"56"},
